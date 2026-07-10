4016b2aed96b85c5a700a5a12f1169c24c5ad92a
Jason2g27 <146676938+Jason2g27@users.noreply.github.com>
2026-07-10 14:47:06 -0500

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> cur;
+        traverse(nums, target, 0, cur);
+        return result;
+    }
+
+    void traverse(vector<int> nums, int target, int i, vector<int>& cur){
+        int sum = 0;
+        for(int j : cur){
+            sum+=j;
+        }
+        if(sum > target){
+            return;
+        }
+        if(sum == target){
+            result.push_back(cur);
+            return;
+        }
+        if(i == nums.size()){
+            return;
+        }
+        int next = nums[i];
+        cur.push_back(next);
+        traverse(nums, target, i, cur);
+        cur.pop_back();
+        traverse(nums, target, i + 1, cur);
+    }
+};
